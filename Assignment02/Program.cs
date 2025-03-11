1e4a14ed29f204f8201e1b4065d0c5cb61ac9f90
moohammedddd <midorafat115566@gmail.com>
2025-03-11 03:23:23 +0200

    Q3

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -19,9 +19,16 @@ namespace Assignment02
             #endregion
 
             #region Q2  2. Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
-            var result = ProductList.FirstOrDefault(p => p.UnitPrice > 1000);
+            //var result = ProductList.FirstOrDefault(p => p.UnitPrice > 1000);
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Q3 Retrieve the second number greater than 5 
+            int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var result = Arr.Where(x => x > 5).ElementAt(1);
             Console.WriteLine(result);
             #endregion
+
             #endregion
 
         }
